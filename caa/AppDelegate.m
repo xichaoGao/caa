d43b2b3625c844edad06f9d870926442e275138a
xichao <402882461@qq.com>
2016-11-29 15:55:37 +0800

    耿

--- caa/AppDelegate.m
+++ caa/AppDelegate.m
@@ -301,7 +301,7 @@ static BOOL const isProduction = FALSE; // 极光TRUE为生产环境
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     
-    
+     [[NSNotificationCenter defaultCenter]postNotificationName:@"Refresh" object:nil];
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
